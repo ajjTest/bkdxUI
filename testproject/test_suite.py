a9cd6142f7d053631af72a7e8a23d317bafb024a
juan <1120837962@qq.com>
2020-03-12 15:23:41 +0800

    Signed-off-by: juan <1120837962@qq.com>

--- testproject/test_suite.py
+++ testproject/test_suite.py
@@ -6,7 +6,7 @@ from BeautifulReport import BeautifulReport
 if __name__ == '__main__':
     test_suite = unittest.defaultTestLoader.discover('casetests', pattern='aaa.py')
     result = BeautifulReport(test_suite)
-    result.report(filename='测试报告', description='测试deafult报告', report_dir='E:\workplace/bkdxweb/baokaodaxue/testproject/reports', theme='theme_default')
+    result.report(filename='测试报告', description='报考大学UI测试报告', report_dir='E:\workplace/bkdxweb/baokaodaxue/testproject/reports', theme='theme_default')
 
 
 
