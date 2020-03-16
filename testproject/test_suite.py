6c6016909544a980fdc946cf5d77559a7fb013e0
juan <1120837962@qq.com>
2020-03-16 14:27:28 +0800

    Signed-off-by: juan <1120837962@qq.com>

--- testproject/test_suite.py
+++ testproject/test_suite.py
@@ -2,9 +2,10 @@ import unittest
 import time
 import sys
 from BeautifulReport import BeautifulReport
-
+sys.path.append('common')
+from location import Locations
 if __name__ == '__main__':
-    test_suite = unittest.defaultTestLoader.discover('casetests/login', pattern='login.py')
+    test_suite = unittest.defaultTestLoader.discover('teseCases/login', pattern='login.py')
     result = BeautifulReport(test_suite)
     result.report(filename='测试报告', description='报考大学UI测试报告1', report_dir='E:\workplace/bkdxweb/baokaodaxue/testproject/reports', theme='theme_default')
 
